39a5de537b8e64b0d895843c290d82e7432945d1
Paul Miller <jettero@pixie.vhb.voltar.org>
2019-07-01 10:10:54 -0400

    now we're talkin

--- pcf/app.py
+++ pcf/app.py
@@ -2,7 +2,6 @@
 # coding: utf-8
 
 import urwid
-import time
 from pcf.fluidsynth import FluidSynth
 from pcf.misc import PathItem
 
@@ -13,45 +12,72 @@ def get_fso():
         FluidSynthObj = FluidSynth()
     return FluidSynthObj
 
-InstTree_t = InstTree = None
-def get_tree(*k, cache_timeout=1):
-    global InstTree_t, InstTree
-    now = time.time()
-    if InstTree is None or (now - InstTree_t) >= cache_timeout:
-        InstTree = dict()
-        InstTree['/'] = top = FluidNode('FluidSynth')
-        fso = get_fso()
-        for font,name,path in fso.fonts:
-            n = FluidNode(path, font, parent=top)
-            InstTree[n.path] = n
-        for name,font,bank,prog in fso.instruments:
-            fp = f'/{font}'
-            bp = fp + f'/{bank}'
-            if bp not in InstTree:
-                InstTree[bp] = FluidNode(f'Bank-{bank}', font,bank, parent=InstTree[fp])
-            n = FluidNode(name, font,bank,prog, parent=InstTree[bp])
-            InstTree[n.path] = n
-        InstTree_t = now
-    if k:
-        return tuple( InstTree.get(x) for x in k )
-    return InstTree
+def fetch_instrument_tree():
+    global InstTree
+    InstTree = dict()
+    InstTree['/'] = top = FluidNode('FluidSynth')
+    fso = get_fso()
+    for font,name,path in fso.fonts:
+        n = FluidNode(path, font, parent=top)
+        InstTree[n.path] = n
+    for name,font,bank,prog in fso.instruments:
+        fp = f'/{font}'
+        bp = fp + f'/{bank}'
+        if bp not in InstTree:
+            InstTree[bp] = FluidNode(f'Bank-{bank}', font,bank, parent=InstTree[fp])
+        n = FluidNode(name, font,bank,prog, parent=InstTree[bp])
+        InstTree[n.path] = n
+InstTree = None
 
 class FluidWidget(urwid.TreeWidget):
-    pass
+    unexpanded_icon = urwid.AttrMap(urwid.TreeWidget.unexpanded_icon, 'dirmark')
+    expanded_icon = urwid.AttrMap(urwid.TreeWidget.expanded_icon, 'dirmark')
+
+    def __init__(self, node):
+        super().__init__(node)
+        self._w = urwid.AttrWrap(self._w, None)
+        self.flagged = False
+        self.update_w()
+
+    def selectable(self):
+        return True
+
+    def keypress(self, size, key):
+        key = super().keypress(size, key)
+        if key:
+            key = self.unhandled_keys(size, key)
+        return key
+
+    def unhandled_keys(self, size, key):
+        if key == " ":
+            self.flagged = not self.flagged
+            self.update_w()
+        else:
+            return key
+
+    def update_w(self):
+        if self.flagged:
+            self._w.attr = 'flagged'
+            self._w.focus_attr = 'flagged focus'
+        else:
+            self._w.attr = 'body'
+            self._w.focus_attr = 'focus'
 
 class FluidNode(urwid.ParentNode, PathItem):
     attrlist = ('name', 'font', 'bank', 'prog')
 
     def __init__(self, name='FluidSynth', font=None, bank=None, prog=None, parent=None):
+        if InstTree is None:
+            fetch_instrument_tree()
         PathItem.__init__(self, font, bank, prog)
         urwid.ParentNode.__init__(self, name, key=self.path, parent=parent)
         self.kids = list()
 
     def load_child_keys(self):
-        return tuple( k for k,v in get_tree().items() if str(v.get_parent()) == str(self) )
+        return tuple( k for k,v in InstTree.items() if str(v.get_parent()) == str(self) )
 
     def load_child_node(self, key):
-        return get_tree(key)[0]
+        return InstTree[key]
 
     def load_widget(self):
         return FluidWidget(self)
@@ -63,6 +89,16 @@ class PCFApp:
     palette = [ ('body', 'light gray', 'default'),
                 ('head', 'yellow', 'dark blue'),
                 ('foot', 'white', 'dark blue'),
+
+                ('dirmark', 'black', 'dark cyan', 'bold'),
+                ('focus', 'light gray', 'dark blue', 'standout'),
+                ('flagged', 'black', 'dark green', ('bold','underline')),
+                ('focus', 'light gray', 'dark blue', 'standout'),
+                ('flagged focus', 'yellow', 'dark cyan',
+                    ('bold','standout','underline')),
+                ('key', 'light cyan', 'black','underline'),
+                ('title', 'white', 'black', 'bold'),
+                ('error', 'dark red', 'light gray'),
             ]
 
     def __init__(self):
