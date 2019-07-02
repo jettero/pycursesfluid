1250da8fb4b4458c3d69846fb3b84c12c384f47e
Paul Miller <jettero@pixie.vhb.voltar.org>
2019-07-02 08:22:32 -0400

    better log format

--- lrun.py
+++ lrun.py
@@ -4,7 +4,8 @@
 import logging
 import pcf.cmd
 
-logging.basicConfig(level=logging.DEBUG, filename='debug.log')
+logging.basicConfig( level=logging.DEBUG, filename='debug.log',
+    format='%(asctime)s %(name)17s %(levelname)5s %(message)s', datefmt='%H:%M:%S')
 
 if __name__ == '__main__':
     pcf.cmd.run()
