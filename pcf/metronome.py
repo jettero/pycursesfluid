346f3a61dc4f7a37425f1e24be7e011cfef7e7f9
Paul Miller <jettero@wisp.vhb.voltar.org>
2019-08-30 10:52:58 -0400

    goom

--- pcf/metronome.py
+++ pcf/metronome.py
@@ -10,20 +10,26 @@ log = logging.getLogger('pcf.metronome')
 from pcf.beatclock import BeatClock
 
 class Metronome:
-    def __init__(self, channel=2, beats_per_minute=60, *notes):
+    def __init__(self, *notes, channel=1, beats_per_minute=120):
         ''' channel and beats_per_minute are hopefully self explanatory
             notes is a list of notes to play … by number … 60 being middle-c
+            notes can also be tuples themselves … second value is velocity, eg
+            all c, but different velocities
+
+            m = Metronome( (60,120), (60,90), (60,90) ) # nice little waltz
+
         '''
-        self.channel = channel-1
-        if self.channel < 0:
-            self.channel = 0
+        if not (0 <= channel <= 15):
+            channel = 1
+        self.channel = channel
 
         if not notes:
             notes = (60,)
         self.notes = notes
         self.npos = 0
 
-        self.beat_duration = 0.9 * (1.0 / beats_per_minute)
+        self.beat_duration = (0.95 / beats_per_minute)
+
         self.beatclock = BeatClock(callback=self.fire, beats_per_minute=beats_per_minute)
         self.midiout = rtmidi.MidiOut()
         self.midiout.open_virtual_port("pcf.metronome")
@@ -31,8 +37,15 @@ class Metronome:
         if not (0 < self.beat_duration < 1):
             self.beat_duration = 0.1
 
+        self.condition = False
+
     def start(self):
+        self.condition = True
         self.beatclock.start()
+
+    def stop(self):
+        self.condition = False
+        self.beatclock.stop()
 
     def fire(self):
         # the debug logging feels useful for debugging, but the logging methods
@@ -41,10 +54,15 @@ class Metronome:
         # need for it.
 
         note = self.notes[self.npos]
-      # log.debug('pcf.metronome.fire() [on]')
-        self.midiout.send_message([0x90 + self.channel, note, 112])
-        time.sleep(self.beat_duration)
-      # log.debug('pcf.metronome.fire() [off]')
-        self.midiout.send_message([0x80 + self.channel, note, 112])
+        vel = 112
 
-        return True
+        if isinstance(note, (list,tuple)):
+            note,vel = note
+
+        self.npos = (self.npos + 1) % len(self.notes)
+
+        self.midiout.send_message([0x90 + self.channel, note, vel])
+        time.sleep(self.beat_duration)
+        self.midiout.send_message([0x80 + self.channel, note, 0])
+
+        return self.condition
